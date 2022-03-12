90ee47e0cf2cb078ce25bf90f634e1eb42217dfe
Mark Evans <msteven.evans@gmail.com>
2022-03-12 01:03:42 -0700

    ticketing for rave

--- rave.rb
+++ rave.rb
@@ -1,6 +1,8 @@
 class Rave
-  attr_reader :capacity, :duration, :start_time, :end_time, :max_dealers, :max_security
-
+  attr_reader :capacity, :duration, :start_time, :end_time, :max_dealers, :max_security,
+  :available_tickets, :ticket_cost, :event_safe
+  # Maybe the constants DEALER_RATIO and SECURITY_RATIO should be variables unique
+  # to each instance?  Could create some interesting scenarios, high risk, high reward.
   DEALER_RATIO = 50
   SECURITY_RATIO = 10
   LOCATIONS = {
@@ -14,12 +16,53 @@ class Rave
     187 => :jail
   }
 
-  def initialize(capacity: 50, duration: 1200, start_time:)
+  def initialize(start_time:, deposit_wallet:, capacity: 50, duration: 1200, ticket_cost: 25)
     @capacity = capacity # 50 default
     @duration = duration # 1200 sec / 20 minutes
     @start_time = start_time.utc
     @end_time = Time.at(start_time.to_i + duration).utc
     @max_dealers = @capacity / DEALER_RATIO
     @max_security = @capacity / SECURITY_RATIO
+    @dealers = 0
+    @security = 0
+    @ticket_cost = ticket_cost
+    @available_tickets = @max_dealers
+    @event_safe = fund_event_safe(deposit_wallet: deposit_wallet)
+  end
+
+  def buy_ticket(payment_wallet:, quantity: 1, type: :general_admission)
+    return false if sold_out?
+
+    case type
+    when :general_admission
+      return false if @available_tickets - quantity < 0
+      # Intentionally janky and verbose - rewrite, rethink
+      # record the inital balance (we aren't exactly addressing double-spend)
+      inital_balance = payment_wallet.balance
+      # can they afford it?
+      order_total = @ticket_cost * quantity
+      return false if order_total > inital_balance
+      # if they can, charge it and decrement the tickets
+      @available_tickets -= quantity
+      # return the difference of initial_balance and ticket_cost to the wallet
+      payment_wallet.balance = inital_balance - order_total
+      # deposit the difference between the initial balance and ending balance
+      @event_safe += inital_balance - payment_wallet.balance
+      true
+    when :vip
+    else
+    end
+  end
+
+  def refund_ticket
+    # raise SuckerError < StandardError; end
+  end
+
+  def fund_event_safe(deposit_wallet:)
+    0
+  end
+
+  def sold_out?
+    @available_tickets.zero?
   end
 end
--- test/rave_test.rb
+++ test/rave_test.rb
@@ -6,8 +6,9 @@ require_relative "../rave"
 
 class RaveTest < Minitest::Test
   def setup
+    wallet = OpenStruct.new(balance: 5000)
     @capacity = 555
-    @rave = Rave.new(capacity: @capacity, start_time: Time.now.utc)
+    @rave = Rave.new(capacity: @capacity, start_time: Time.now.utc, deposit_wallet: wallet)
   end
 
   def test_instance_of_rave
@@ -36,5 +37,27 @@ class RaveTest < Minitest::Test
     assert_instance_of Time, @rave.start_time
     assert_instance_of Time, @rave.end_time
     assert_instance_of Integer, @rave.duration
+  end
+
+  def test_buy_ticket
+    wallet = OpenStruct.new({ balance: 250 })
+    tickets_before = @rave.available_tickets
+    @rave.buy_ticket(type: :general_admission, quantity: 1, payment_wallet: wallet)
+
+    assert_equal tickets_before, @rave.available_tickets + 1
+    assert_equal tickets_before - 1, @rave.available_tickets
+  end
+
+  def test_sold_out
+    tickets_to_buy = @rave.available_tickets
+    beginning_balance = tickets_to_buy * @rave.ticket_cost * 2
+    wallet = OpenStruct.new({ balance: beginning_balance })
+
+    refute @rave.sold_out?
+    assert @rave.buy_ticket(quantity: tickets_to_buy, payment_wallet: wallet)
+    assert @rave.sold_out?
+    refute @rave.buy_ticket(payment_wallet: wallet)
+    assert beginning_balance > wallet.balance
+    assert @rave.event_safe == beginning_balance - wallet.balance
   end
 end
